1a59760462d1d671b4b1ec723de50d7407b80ec6
quilix11 <nazarus8095@gmail.com>
2026-05-23 17:03:57 +0300

    create: make user schema

--- schemas/user_models.py
+++ schemas/user_models.py
@@ -1,1 +1,11 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr, SecretStr
+
+
+class UserCreate(BaseModel):
+    email: EmailStr
+    password: SecretStr
+
+
+class UserOut(BaseModel):
+    id: int
+    email: EmailStr
--- schemas/website_models.py
+++ schemas/website_models.py
@@ -1,5 +1,4 @@
-from pydantic import BaseModel
-
+from pydantic import BaseModel, ConfigDict
 
 class WebsiteCreate(BaseModel):
     url: str
@@ -12,3 +11,5 @@ class WebsiteOut(BaseModel):
     interval: int
     is_active: bool
     user_id: int
+
+    model_config = ConfigDict(from_attributes=True)
